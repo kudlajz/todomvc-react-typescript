8f7e3c66dccb07a9b1b0533627af8192de999ea9
Marko Grešak <marko@gresak.io>
2019-01-25 19:17:28 +0100

    Add TodoMVC template

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'todomvc-common/base.css';
+import 'todomvc-app-css/index.css';
+import 'todomvc-common/base';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
